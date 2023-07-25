c10c40fbaa834ee70c2a9bf82c3cfccfdfa1b546
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-07-25 12:06:38 +0100

    About to implement adding friends to the array

--- src/App.js
+++ src/App.js
@@ -96,13 +96,31 @@ function Friend({friend, selectedFriend}) {
 }
 
 function AddFriends() {
+  const [name, setName] = useState('');
+  const [img, setImg] = useState('https://i.pravatar.cc/48');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !img) return;
+    const id = crypto.randomUUID();
+    const newfriend = {
+      id,
+      name,
+      image: `${img}?=${id}`,
+      balance: 0,
+    };
+
+    setName('');
+    setImg('https://i.pravatar.cc/48'); // [...initialFriends, newfriend];
+  }
   return (
-    <form>
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👫 Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
 
       <label>🌄 Image URL</label>
-      <input type="text" />
+      <input type="text" value={img} onChange={e => setImg(e.target.value)} />
 
       <Button>Add</Button>
     </form>
